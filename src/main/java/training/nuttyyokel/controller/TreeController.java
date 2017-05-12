892a214a58f5958f9b666d758b04dfb6f723d3f1
Marton Cserei <marton.cserei@iquestgroup.com>
2017-05-12 11:36:10 +0300

    switched to more explicit mapping

--- src/main/java/training/nuttyyokel/controller/TreeController.java
+++ src/main/java/training/nuttyyokel/controller/TreeController.java
@@ -1,14 +1,16 @@
 package training.nuttyyokel.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/tree")
 public class TreeController {
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String home() {
+        System.out.println("Called home");
         return "Trees";
     }
 }
