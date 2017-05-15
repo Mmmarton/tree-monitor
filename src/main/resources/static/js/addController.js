82135f9491ac68e09042fa38d42f1119f7456be2
Marton <warcodhiro@gmail.com>
2017-05-16 00:06:22 +0200

    creating Add post

--- src/main/resources/static/js/addController.js
+++ src/main/resources/static/js/addController.js
@@ -1,3 +1,16 @@
 mainApp.controller('addController', function ($scope, $http) {
-    
+  $scope.save = function () {
+    console.log("2");
+    var request = {
+      method: 'POST',
+      url: '"/api/tree/"',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: $scope.tree
+    }
+    $http.post(request).then(function (response) {
+      alert(response);
+    });
+  }
 })
--- src/main/resources/static/js/indexController.js
+++ src/main/resources/static/js/indexController.js
@@ -1,12 +1,12 @@
 mainApp.controller('indexController', function ($scope, $http, ngDialog) {
-  $http.get("http://localhost:8080/api/tree/").then(function (response) {
+  $http.get("/api/tree/").then(function (response) {
     $scope.trees = response.data;
   });
   $scope.orderBy = function (field) {
     $scope.orderField = field;
   }
 
-  $scope.clickToOpen = function () {
+  $scope.openAddModal = function () {
     ngDialog.open({template: 'add.html', className: 'ngdialog-theme-default'});
   };
 })
