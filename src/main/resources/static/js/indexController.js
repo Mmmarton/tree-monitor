456b6e412a19b65d2916677f4ad37578ddae068b
Marton <warcodhiro@gmail.com>
2017-05-15 23:39:53 +0200

    moved Add into modal

--- src/main/resources/static/js/indexController.js
+++ src/main/resources/static/js/indexController.js
@@ -1,8 +1,12 @@
-mainApp.controller('indexController', function ($scope, $http) {
-    $http.get("http://localhost:8080/api/tree/").then(function (response) {
-        $scope.trees = response.data;
-    });
-    $scope.orderBy = function (field) {
-        $scope.orderField = field;
-    }
+mainApp.controller('indexController', function ($scope, $http, ngDialog) {
+  $http.get("http://localhost:8080/api/tree/").then(function (response) {
+    $scope.trees = response.data;
+  });
+  $scope.orderBy = function (field) {
+    $scope.orderField = field;
+  }
+
+  $scope.clickToOpen = function () {
+    ngDialog.open({template: 'add.html', className: 'ngdialog-theme-default'});
+  };
 })
